260fddf654684432262d355f1539f454d5b07d4c
donacesar <donacesar@gmail.com>
2023-05-12 08:53:49 +0300

    some fix4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        dd(Hash::make('Donacesar1'));
          \App\Models\User::factory(1)->create();
 
         // \App\Models\User::factory()->create([
