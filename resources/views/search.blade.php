7fab173d6393da1bb60fcfa95e449e14ccdd7828
donacesar <donacesar@gmail.com>
2023-07-17 14:50:28 +0300

    remove admin links in search response

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,7 +5,7 @@
         @if(empty($posts))
             <div class="main">
                 <div class="message-box">
-                    <div class="text-box">Ничего не найдено...</div>
+                    <div class="text-box">Ничего не найдено</div>
                 </div>
             </div>
 
@@ -41,98 +41,6 @@
                             @endif
 
                         </div>
-
-                        @auth()
-
-                            <div class="edit_menu_wrapper">
-
-                                <nav class="edit_menu_wide" id="edit_menu{{$post->id}}">
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".up", $post->id) }}"
-                                           class="nav-link pe-2"><i
-                                                class="fa-solid fa-circle-chevron-up ms-2"></i></a>
-
-                                    </div>
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".down", $post->id) }}"
-                                           class="nav-link pe-2"><i
-                                                class="fa-solid fa-circle-chevron-down ms-2"></i></a>
-
-                                    </div>
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".edit", $post->id) }}"
-                                           class="nav-link pe-2"><i
-                                                class="fa-solid fa-pen-to-square ms-2"></i></a>
-
-                                    </div>
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".delete", $post->id) }}"
-                                           data-method="delete"
-                                           class="nav-link"><i class="fa-solid fa-trash ms-2 me-2"></i></a>
-
-                                    </div>
-
-                                </nav>
-
-
-                                <button class="edit-burger" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseExample{{ $post->id }}"
-                                        aria-expanded="false" aria-controls="collapseExample{{ $post->id }}"
-                                        id="edit-burger{{$post->id}}">
-
-                                    <div class="edit-burger-line"></div>
-                                    <div class="edit-burger-line"></div>
-                                    <div class="edit-burger-line"></div>
-
-                                </button>
-
-                                <nav class="edit_menu" id="edit_menu{{$post->id}}">
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".up", $post->id) }}"
-                                           class="nav-link pe-2"><i
-                                                class="fa-solid fa-circle-chevron-up ms-2"></i></a>
-
-                                    </div>
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".down", $post->id) }}"
-                                           class="nav-link pe-2"><i
-                                                class="fa-solid fa-circle-chevron-down ms-2"></i></a>
-
-                                    </div>
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".edit", $post->id) }}"
-                                           class="nav-link pe-2"><i
-                                                class="fa-solid fa-pen-to-square ms-2"></i></a>
-
-                                    </div>
-
-                                    <div class="edit_menu_item">
-
-                                        <a href="{{ route($route_name . ".delete", $post->id) }}"
-                                           data-method="delete"
-                                           class="nav-link"><i class="fa-solid fa-trash ms-2 me-2"></i></a>
-
-                                    </div>
-
-                                </nav>
-
-                            </div>
-
-                        @endauth
 
                     </div>
 
