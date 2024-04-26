38730361a410de92b8a77c88e8b55873ec1c0f37
donacesar <donacesar@gmail.com>
2024-04-26 18:07:29 +0300

    update pagination links work

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -42,6 +43,8 @@ class AppServiceProvider extends ServiceProvider
                 ]
             );
         });
+
+        Schema::defaultStringLength(191);
 
 
     }
